886d7926f74d52f5f6c5901edfdad4d92ebf7fd5
Dmitry Pechenin <pecheneg@student.su>
2024-12-16 00:04:41 +0300

    day 15. done. need refactoring

--- day_15/solution.ts
+++ day_15/solution.ts
@@ -2,19 +2,19 @@ import { getRawLines } from "#utils";
 
 type Point = [number, number];
 const dirs: Point[] = [[-1, 0], [0, 1], [1, 0], [0, -1]];
-let dirKeyMap:Record<string,number>= {
-  "^":0,
-  ">":1,
-  "v":2,
-  "<":3,
-}
+let dirKeyMap: Record<string, number> = {
+  "^": 0,
+  ">": 1,
+  "v": 2,
+  "<": 3,
+};
 
-let invMap:Record<string,string>= {
-  "^":"v",
-  ">":"<",
-  "v":"^",
-  "<":">",
-}
+let invMap: Record<string, string> = {
+  "^": "v",
+  ">": "<",
+  "v": "^",
+  "<": ">",
+};
 function getArr(data: string[]): string[][] {
   const arr: string[][] = [];
   for (let i = 0; i < data.length; i++) {
@@ -37,8 +37,7 @@ function getActions(data: string[]): string[] {
       arr.push(data[i]);
     }
   }
-
-  return arr.join("").split("");
+  return arr.join("").split("").filter((e) => ["^", ">", "v", "<"].includes(e));
 }
 
 function getStartPoint(data: string[][]): Point {
@@ -53,41 +52,45 @@ function getStartPoint(data: string[][]): Point {
 }
 
 function caclRes(data: string[][]): number {
-  let res =0
+  let res = 0;
   for (let i = 0; i < data.length; i++) {
     for (let j = 0; j < data[0].length; j++) {
       if (data[i][j] === "O") {
-       res+=i*100+j
+        res += i * 100 + j;
       }
     }
   }
-  return res
+  return res;
 }
 
-function updateField(data: string[][], current: Point, dirKey: number,invDirKey:number):Point {
+function updateField(
+  data: string[][],
+  current: Point,
+  dirKey: number,
+): Point {
   let dir = dirs[dirKey];
-  let dirInv = dirs[invDirKey]
   let currentPoint = [...current];
-  while (data[currentPoint[0]][currentPoint[1]] !== "#") {
+  let boxCoord: null | Point = null;
+
+  while (true) {
     currentPoint = [currentPoint[0] + dir[0], currentPoint[1] + dir[1]];
-    if(data[currentPoint[0]][currentPoint[1]]==="."){ 
+    if (data[currentPoint[0]][currentPoint[1]] === "#") {
+      return current;
+    }
+    if (data[currentPoint[0]][currentPoint[1]] === "O" && !boxCoord) {
+      boxCoord = [...currentPoint] as Point;
+    }
+    if (data[currentPoint[0]][currentPoint[1]] === ".") {
       break;
     }
   }
-  if(data[currentPoint[0]][currentPoint[1]] === "#")
-    return current;
-  let res = [...current]  as Point
-  while(`${currentPoint[0]}_${currentPoint[1]}` !== `${current[0]}_${current[1]}`){
-
-    res=[...currentPoint] as Point
-    let newCoord = [currentPoint[0] + dirInv[0], currentPoint[1] + dirInv[1]]
-    let t = data[currentPoint[0]][currentPoint[1]]
-    data[currentPoint[0]][currentPoint[1]] = data[newCoord[0]][newCoord[1]]
-    data[newCoord[0]][newCoord[1]] = t
-    currentPoint= newCoord
+  if (boxCoord) {
+    data[currentPoint[0]][currentPoint[1]] = "O";
+    data[boxCoord[0]][boxCoord[1]] = ".";
+    return boxCoord;
   }
-  // data.map(e=>{console.log(e.join(''))})
-  return res
+
+  return [current[0] + dir[0], current[1] + dir[1]];
 }
 
 function firstPartSolution(
@@ -95,28 +98,228 @@ function firstPartSolution(
   start: Point,
   actions: string[],
 ): number {
-  let current = start
+  let current = start;
   actions.forEach((action) => {
-    current =updateField(data,current,dirKeyMap[action],dirKeyMap[invMap[action]])
+    current = updateField(
+      data,
+      current,
+      dirKeyMap[action],
+    );
   });
-  // data.map(e=>{console.log(e.join(''))})
+  data.map((e) => {
+    console.log(e.join(""));
+  });
+
   return caclRes(data);
 }
 
-function secondPartSolution(data: string[]): number {
-  return 0;
+function getArrPart2(data: string[][]): string[][] {
+  const arr: string[][] = [];
+  for (let i = 0; i < data.length; i++) {
+    arr.push([]);
+    for (let j = 0; j < data[0].length; j++) {
+      if (data[i][j] === "#") {
+        arr[i].push("#");
+        arr[i].push("#");
+      }
+      if (data[i][j] === ".") {
+        arr[i].push(".");
+        arr[i].push(".");
+      }
+      if (data[i][j] === "@") {
+        arr[i].push("@");
+        arr[i].push(".");
+      }
+      if (data[i][j] === "O") {
+        arr[i].push("[");
+        arr[i].push("]");
+      }
+    }
+  }
+
+  return arr;
+}
+
+function updateFieldH(
+  data: string[][],
+  current: Point,
+  dir: Point,
+  invDir: Point,
+): Point {
+  console.log("c", current);
+  let currentPoint: Point = [...current];
+
+  while (true) {
+    currentPoint = [currentPoint[0] + dir[0], currentPoint[1] + dir[1]];
+    if (data[currentPoint[0]][currentPoint[1]] === "#") {
+      return current;
+    }
+
+    if (data[currentPoint[0]][currentPoint[1]] === ".") {
+      break;
+    }
+  }
+  let tempPoint: Point = [...currentPoint];
+  while (`${current[0]}_${current[1]}` !== `${tempPoint[0]}_${tempPoint[1]}`) {
+    let newCoord: Point = [tempPoint[0] + invDir[0], tempPoint[1] + invDir[1]];
+    let t = data[tempPoint[0]][tempPoint[1]];
+    data[tempPoint[0]][tempPoint[1]] = data[newCoord[0]][newCoord[1]];
+    data[newCoord[0]][newCoord[1]] = t;
+    tempPoint = newCoord;
+  }
+  return [current[0] + dir[0], current[1] + dir[1]];
+}
+function calcRes2(data: string[][]): number {
+  let res = 0;
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[0].length; j++) {
+      if (data[i][j] === "[") {
+        res += i * 100 + j;
+      }
+    }
+  }
+  return res;
+}
+function updateFieldVT(
+  data: string[][],
+  current: Point,
+  dir: Point,
+): Point {
+  let stack: Point[] = [current];
+  let isWrong = false;
+  let visitedArr: string[] = [];
+  while (stack.length) {
+    console.log("r");
+    let el = stack.shift() as Point;
+    visitedArr.push(`${el[0]}_${el[1]}`);
+    let newElVal = data[el[0] - 1][el[1]];
+
+    if (newElVal === "[") {
+      stack.push([el[0] - 1, el[1]]);
+      stack.push([el[0] - 1, el[1] + 1]);
+    }
+    if (newElVal === "]") {
+      stack.push([el[0] - 1, el[1]]);
+      stack.push([el[0] - 1, el[1] - 1]);
+    }
+    if (newElVal === "#") {
+      console.log("wr", el);
+      isWrong = true;
+      break;
+    }
+  }
+  if (isWrong) {
+    return current;
+  }
+  let visited: Point[] = Array.from(new Set(visitedArr)).map(e=>e.split('_').map(e=>+e) as Point);
+
+  visited.sort((a, b) => a[0] - b[0]);
+  visited.forEach((point) => {
+    let val = data[point[0]][point[1]];
+    data[point[0]][point[1]] = data[point[0] - 1][point[1]];
+    data[point[0] - 1][point[1]] = val;
+  });
+  return [current[0] + dir[0], current[1] + dir[1]];
+}
+
+function updateFieldVB(
+  data: string[][],
+  current: Point,
+  dir: Point,
+): Point {
+  let stack: Point[] = [current];
+  let isWrong = false;
+  let visitedArr: string[] = [];
+  while (stack.length) {
+    console.log("r");
+    let el = stack.shift() as Point;
+    visitedArr.push(`${el[0]}_${el[1]}`);
+    let newElVal = data[el[0] + 1][el[1]];
+
+    if (newElVal === "[") {
+      stack.push([el[0] + 1, el[1]]);
+      stack.push([el[0] + 1, el[1] + 1]);
+    }
+    if (newElVal === "]") {
+      stack.push([el[0] + 1, el[1]]);
+      stack.push([el[0] + 1, el[1] - 1]);
+    }
+    if (newElVal === "#") {
+      console.log("wr", el);
+      isWrong = true;
+      break;
+    }
+  }
+  if (isWrong) {
+    return current;
+  }
+  let visited: Point[] = Array.from(new Set(visitedArr)).map(e=>e.split('_').map(e=>+e) as Point);
+
+  visited.sort((a, b) => b[0] - a[0]);
+  visited.forEach((point) => {
+    let val = data[point[0]][point[1]];
+    data[point[0]][point[1]] = data[point[0] + 1][point[1]];
+    data[point[0] + 1][point[1]] = val;
+  });
+  return [current[0] + dir[0], current[1] + dir[1]];
+}
+function secondPartSolution(
+  data: string[][],
+  start: Point,
+  actions: string[],
+): number {
+  let current = start;
+  actions.forEach((action) => {
+    // console.log(action);
+    // data.map((e) => {
+    //   console.log(e.join(""));
+    // });
+    if (action === "<" || action === ">") {
+      current = updateFieldH(
+        data,
+        current,
+        dirs[dirKeyMap[action]],
+        dirs[dirKeyMap[invMap[action]]],
+      );
+    } else {
+      if (action === "^") {
+        current = updateFieldVT(
+          data,
+          current,
+          dirs[dirKeyMap[action]],
+        );
+      }
+      if (action === "v") {
+        current = updateFieldVB(
+          data,
+          current,
+          dirs[dirKeyMap[action]],
+        );
+      }
+    }
+  });
+  data.map((e) => {
+    console.log(e.join(""));
+  });
+  return calcRes2(data);
 }
 
 const main = async () => {
-  const rawData = await getRawLines("./day_15/data/example.txt");
+  const rawData = await getRawLines("./day_15/data/input.txt");
   const field = getArr(rawData);
   const actionList = getActions(rawData);
   const startPoint: Point = getStartPoint(field);
-  field[startPoint[0]][startPoint[1]] = ".";
-  console.log(startPoint);
-  // const data = rawData.map(prepareData);
-  console.log("Part 1: ", firstPartSolution(field,startPoint,actionList));
-  // console.log("Part 2: ", secondPartSolution(data));
+  // field[startPoint[0]][startPoint[1]] = ".";
+  let fieldV2 = getArrPart2(field);
+  const startPointV2: Point = getStartPoint(fieldV2);
+  console.log(startPointV2);
+  // fieldV2[startPointV2[0]][startPointV2[1]] = ".";
+
+  // console.log("Part 1: ", firstPartSolution(field, startPoint, actionList));
+  console.log(
+    "Part 2: ",
+    secondPartSolution(fieldV2, startPointV2, actionList),
+  );
 };
-// res < 1433073
+
 main();
