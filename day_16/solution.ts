fa5837e233ff83266eff6c8cbdca38a5024407f7
Dmitry Pechenin <pecheneg@student.su>
2024-12-16 18:29:02 +0300

    wip

--- day_16/solution.ts
+++ day_16/solution.ts
@@ -65,17 +65,17 @@ function firstPartSolution(
   end: Point,
 ): number {
   let res: number[] = [];
-  let minRes = data.length * data[0].length * 100;
-  console.log(  data.length *data[0].length * 100)
+  let minRes = data.length * data[0].length * 100/2;
+  console.log(  data.length *data[0].length * 100/2)
   let stack: StackEl[] = [[start, "E", 0, `${start[0]}_${start[1]}_E`],[start, "N", 1000, `${start[0]}_${start[1]}_N`]];
   while (stack.length) {
-    let [point, dir, sum, visited] = stack.shift() as StackEl;
+    let [point, dir, sum, visited] = stack.pop() as StackEl;
     if (data[point[0]][point[1]] === "#") {
       continue;
     }
     if (point[0] === end[0] && point[1] === end[1]) {
       res.push(sum);
-      // console.log('ddd',sum)
+      console.log('ddd',sum)
       minRes = Math.min(sum, minRes);
     } else if (sum < minRes) {
       let del = delta[dir];
@@ -132,12 +132,48 @@ function secondPartSolution(
   return 0;
 }
 
+function updateField(data:string[][]){
+  for(let m =0;m<data.length*data[0].length;m++){
+    for(let i=1;i<data.length-1;i++){
+      for(let j=1;j<data[0].length-1;j++){
+        if(data[i][j]==='.'){
+          let topLeft = data[i-1][j-1]==='#'?1:0
+          let top = data[i-1][j]==='#'?1:0
+          let topRight = data[i-1][j+1]==='#'?1:0
+          let left = data[i][j-1]==='#'?1:0
+          let right = data[i][j+1]==='#'?1:0
+          let bottomLeft = data[i+1][j-1]==='#'?1:0
+          let bottom = data[i+1][j]==='#'?1:0
+          let bottomRight = data[i+1][j+1]==='#'?1:0
+          let t = left+ topLeft+top+topRight+right
+          let l = left+ topLeft+bottomLeft+top+bottom
+          let r = right+ topRight+bottomRight+top+bottom
+          let b = left+ bottomLeft+bottom+bottomRight+right
+
+
+          if(t===5 || l===5 || r===5 ||b===5){
+            console.log("UPD")
+                        data[i][j]='#'
+          }
+
+  
+        }
+      }
+    }
+    // data.map(e=>console.log(e.join('')))
+
+  }
+
+}
 const main = async () => {
   const rawData = await getRawLines("./day_16/data/example.txt");
   const field = getArr(rawData);
   const startPoint: Point = findPoint(field, "S");
   const endPoint: Point = findPoint(field, "E");
   console.log(startPoint, endPoint);
+  updateField(field)
+  console.log('updateField',)
+  field.map(e=>console.log(e.join('')))
   console.log("Part 1: ", firstPartSolution(field, startPoint, endPoint));
 };
 
